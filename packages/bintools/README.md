92630e75cd266a783fe244dce1f703ce2b529149
Blayne Chard <blayne@chard.com>
2020-09-19 20:13:04 +1200

    feat(bintools): load data directly from the mpq

--- packages/bintools/README.md
+++ packages/bintools/README.md
@@ -1,14 +1,12 @@
 # @diablo2/bintools
 
-Binary file parsers for some of the `.bin` or `.tbl` files
-
-This currently requires the MPQ to be extracted using something like `smpq` with a diablo2 list file
+Binary file parsers for some of the `.bin` and `.tbl` files
 
 
 ```typescript
 import {Diablo2MpqLoader} from '@diablo2/bintools'
 
-const mpq = await Diablo2MpqLoader.load('Path/To/Extracted/Mpq');
+const mpq = await Diablo2MpqLoader.load('patch_d2.mpq');
 
 // Translate a translation key
 const lang = mpq.t('7cr') // "Phase blade"
@@ -18,5 +16,4 @@ const mon = mpq.monster.getMonsterName(0) // Skeleton
 
 // Get information about super uniques
 const mon = mpq.monster.getSuperUniqueName(3) // Rakanishu
-
 ```
--- packages/bintools/src/mpq.loader.ts
+++ packages/bintools/src/mpq.loader.ts
@@ -1,6 +1,7 @@
 import { Diablo2Mpq, Diablo2MpqData } from '@diablo2/data';
+import { Mpq } from '@diablo2/mpq';
 import { StrutInfer, StrutType } from 'binparse';
-import { existsSync, promises as fs } from 'fs';
+import { promises as fs } from 'fs';
 import * as path from 'path';
 import { ItemFileParser } from './item/item.reader';
 import { LangReader } from './lang/lang.reader';
@@ -8,13 +9,14 @@ import { Logger } from './log.type';
 import { MonsterReader, MonsterReader2 } from './monster/monster.stat.reader';
 
 async function readAndParse<T extends StrutType<any>>(
-  basePath: string,
-  fileName: string,
+  mpq: Mpq,
+  key: string,
   parser: T,
 ): Promise<{ value: StrutInfer<T>; duration: number } | null> {
   try {
     const startTime = Date.now();
-    const bytes = await fs.readFile(path.join(basePath, fileName));
+    const bytes = await mpq.extract(key);
+    if (bytes == null) return null;
     const value = parser.parse(bytes, { offset: 0, startOffset: 0 });
     const duration = Date.now() - startTime;
     return { value, duration };
@@ -27,83 +29,78 @@ async function readAndParse<T extends StrutType<any>>(
 const LangFiles = ['string.tbl', 'expansionstring.tbl', 'patchstring.tbl'];
 // Order matters for item imports
 const ItemFiles = ['Weapons.bin', 'Armor.bin', 'Misc.bin'];
+const MpqFile = 'patch_d2.mpq';
 
 export class Diablo2MpqLoader {
-  static async load(basePath: string, log?: Logger, mpq = Diablo2Mpq): Promise<Diablo2MpqData> {
-    if (!existsSync(path.join(basePath, 'data'))) {
-      log?.error({ basePath: basePath }, 'No /data found, mpq needs to be extracted');
-      throw new Error('Failed to init no MPQ data found');
-    }
+  static async load(basePath: string, log?: Logger, data = Diablo2Mpq): Promise<Diablo2MpqData> {
+    const mpqFilePath = path.join(basePath, MpqFile);
+    log?.info({ mpq: mpqFilePath }, 'Mpq:Load');
 
-    mpq.basePath = basePath;
-    await this.initLang(mpq, log);
-    await this.initMonsters(mpq, log);
-    await this.initItems(mpq, log);
-    return mpq;
+    const mpqData = await fs.readFile(mpqFilePath);
+    const mpqReader = Mpq.load(mpqData);
+
+    data.basePath = basePath;
+    await this.initLang(data, mpqReader, log);
+    await this.initMonsters(data, mpqReader, log);
+    await this.initItems(data, mpqReader, log);
+    return data;
   }
 
   /** Load in the lang files */
-  static async initLang(mpq: Diablo2MpqData, logger?: Logger): Promise<void> {
-    const LangPath = `${mpq.basePath}/data/local/LNG/ENG`;
-
+  static async initLang(data: Diablo2MpqData, mpq: Mpq, logger?: Logger): Promise<void> {
     for (const langFile of LangFiles) {
-      const res = await readAndParse(LangPath, langFile, LangReader);
-      if (res == null) continue;
-
-      const langItems = res.value.hashTable;
-      for (const itm of langItems) {
-        switch (langFile) {
-          case 'string.tbl':
-            mpq.lang.classic.add(itm.key, itm.index, itm.value);
-            break;
-          case 'patchstring.tbl':
-            mpq.lang.patch.add(itm.key, itm.index, itm.value);
-            break;
-          case 'expansionstring.tbl':
-            mpq.lang.expansion.add(itm.key, itm.index, itm.value);
-            break;
-          default:
-            throw new Error('invalid lang file ' + langFile);
-        }
+      const res = await readAndParse(mpq, `data/local/LNG/ENG/${langFile}`, LangReader);
+      if (res == null) {
+        logger?.warn({ file: langFile }, 'Mpq:Load:Failed');
+        continue;
       }
-      logger?.debug({ file: langFile, records: langItems.length, duration: res.duration }, 'MPQ:Load:Language');
+      const langItems = res.value.hashTable;
+      switch (langFile) {
+        case 'string.tbl':
+          for (const itm of langItems) data.lang.classic.add(itm.key, itm.index, itm.value);
+          break;
+        case 'patchstring.tbl':
+          for (const itm of langItems) data.lang.patch.add(itm.key, itm.index, itm.value);
+          break;
+        case 'expansionstring.tbl':
+          for (const itm of langItems) data.lang.expansion.add(itm.key, itm.index, itm.value);
+          break;
+        default:
+          throw new Error('Invalid lang file ' + langFile);
+      }
+
+      logger?.debug({ file: langFile, records: langItems.length, duration: res.duration }, 'Mpq:Load:Language');
     }
   }
 
   /** Load in the lang files */
-  static async initMonsters(mpq: Diablo2MpqData, logger?: Logger): Promise<void> {
-    const BinPath = `${mpq.basePath}/data/global/excel`;
-    const res = await readAndParse(BinPath, 'MonStats.bin', MonsterReader);
-    if (res) {
-      const monsters = res.value.monsters;
-      for (const mon of monsters) {
-        mpq.monsters.add(mon.id, mon);
-      }
-      logger?.debug({ file: 'MonStats.bin', records: monsters.length, duration: res.duration }, 'MPQ:Load:Monster');
-    }
+  static async initMonsters(data: Diablo2MpqData, mpq: Mpq, logger?: Logger): Promise<void> {
+    const res = await readAndParse(mpq, 'data/global/excel/monstats.bin', MonsterReader);
+    if (res == null) return logger?.warn({ file: 'MonStats.bin' }, 'Mpq:Load:Failed');
 
-    const res2 = await readAndParse(BinPath, 'MonStats2.bin', MonsterReader2);
-    if (res2) {
-      const monsters2 = res2.value.monsters;
-      for (const mon of monsters2) {
-        mpq.monsters.addState(mon.id, mon.state);
-      }
-      logger?.debug({ file: 'MonStats2.bin', records: monsters2.length, duration: res2.duration }, 'MPQ:Load:Monster');
-    }
+    const monsters = res.value.monsters;
+    for (const mon of monsters) data.monsters.add(mon.id, mon);
+    logger?.debug({ file: 'MonStats.bin', records: monsters.length, duration: res.duration }, 'Mpq:Load:Monster');
+
+    const res2 = await readAndParse(mpq, 'data/global/excel/MonStats2.bin', MonsterReader2);
+    if (res2 == null) return logger?.warn({ file: 'MonStats2.bin' }, 'Mpq:Load:Failed');
+
+    const monsters2 = res2.value.monsters;
+    for (const mon of monsters2) data.monsters.addState(mon.id, mon.state);
+    logger?.debug({ file: 'MonStats2.bin', records: monsters2.length, duration: res2.duration }, 'Mpq:Load:Monster');
   }
 
-  static async initItems(mpq: Diablo2MpqData, logger?: Logger): Promise<void> {
-    //   // TODO these lang files need to be extracted, would be good to load from the MPQ directly
-    const BinPath = `${mpq.basePath}/data/global/excel`;
-    //
+  static async initItems(data: Diablo2MpqData, mpq: Mpq, logger?: Logger): Promise<void> {
     for (const itemFile of ItemFiles) {
-      const res = await readAndParse(BinPath, itemFile, ItemFileParser);
-      if (res == null) continue;
+      const res = await readAndParse(mpq, `data/global/excel/${itemFile}`, ItemFileParser);
+      if (res == null) {
+        logger?.warn({ file: itemFile }, 'Mpq:Load:Failed');
+        continue;
+      }
       const items = res.value.items;
 
-      for (const item of items) mpq.items.add(item);
-
-      logger?.debug({ file: itemFile, records: items.length, duration: res.duration }, 'MPQ:Load:Items');
+      for (const item of items) data.items.add(item);
+      logger?.debug({ file: itemFile, records: items.length, duration: res.duration }, 'Mpq:Load:Items');
     }
   }
 }
