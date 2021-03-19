67f1badaf6bc739a755c34f777f72d89e784d522
Blayne Chard <blayne@chard.com>
2021-03-19 22:39:46 +1300

    refactor: expose KillJson type

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,3 @@
 export { Diablo2Client } from './client';
 export { Diablo2GameSession } from './game.state';
-export type { GameStateJson, NpcJson, ItemJson } from './state/json';
+export type { GameStateJson, NpcJson, ItemJson, KillJson } from './state/json';
