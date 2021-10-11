150dede7966318f4c2ae7ea65e120236ad149c8a
Blayne Chard <blayne@chard.com>
2021-10-11 22:34:38 +1300

    Revert "wip: look at near by rooms"
    This reverts commit e54c56e68ba80b8dee0b8f653363a223748844a7.

--- packages/memory/src/structures.ts
+++ packages/memory/src/structures.ts
@@ -42,33 +42,11 @@ export const StatListStrut = bp.object('StatList', {
 });
 export type StatList = StrutInfer<typeof StatListStrut>;
 
-const bpUnk = bp.u8;
-
-export const RoomStrut = bp.object('Room1', {
-  roomNear: new Pointer(bpUnk),
-  unk1: bp.array('unk1', new Pointer(bpUnk), 2),
-  room2: new Pointer(bpUnk),
-  unk2: bp.array('unk2', new Pointer(bpUnk), 3),
-  colMap: new Pointer(bpUnk),
-  count: bp.lu32,
-  unk3: bp.skip(15 * 4),
-  x: bp.lu32,
-  y: bp.lu32,
-  width: bp.lu32,
-  height: bp.lu32,
-  unk4: bp.array('unk2', new Pointer(bpUnk), 4),
-  pUnitFirst: new Pointer(bpUnk),
-  unk5: bp.array('unk2', new Pointer(bpUnk), 4),
-  pRoomNext: new Pointer(bpUnk),
-})
-
 export const PathStrut = bp.object('Path', {
   xOffset: bp.lu16,
   x: bp.lu16,
   yOffset: bp.lu16,
   y: bp.lu16,
-  unk1: bp.skip(24),
-  pRoom: new Pointer(RoomStrut)
 });
 export type PathS = StrutInfer<typeof PathStrut>;
 
