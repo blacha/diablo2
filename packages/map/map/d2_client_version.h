9293907498bf5a73df6ca9138b7849230af1cc33
Blayne Chard <blayne@chard.com>
2021-10-22 16:44:36 +1300

    fix(map): unload act if seed or difficulty changes

--- packages/map/map/d2_client_version.h
+++ packages/map/map/d2_client_version.h
@@ -1,12 +1,35 @@
 #include "d2_ptrs.h"
 #include "d2_version.h"
 
-Act* d2common_load_act(D2Version gameVersion, int actId, int seed, int difficulty) {
+/* Cache the recently generated acts if they change unload it */
+Act* acts[5] = {NULL, NULL, NULL, NULL, NULL};
+int act_seeds[5] = {0,0,0,0,0};
+int act_diff[5] = {-1,-1,-1,-1,-1};
+
+Act* d2common_load_act_run(D2Version gameVersion, int actId, int seed, int difficulty) {
     if (gameVersion == VersionPathOfDiablo) return D2COMMON_Pod_LoadAct(actId, seed, TRUE, FALSE, difficulty, (DWORD)NULL, 1, D2CLIENT_Pod_LoadAct_1, D2CLIENT_Pod_LoadAct_2);
     if (gameVersion == VersionProjectDiablo2) return D2COMMON_Pd2_LoadAct(actId, seed, TRUE, FALSE, difficulty, (DWORD)NULL, 1, D2CLIENT_Pd2_LoadAct_1, D2CLIENT_Pd2_LoadAct_2);
     if (gameVersion == VersionDiablo2) return D2COMMON_Pd2_LoadAct(actId, seed, TRUE, FALSE, difficulty, (DWORD)NULL, 1, D2CLIENT_Pd2_LoadAct_1, D2CLIENT_Pd2_LoadAct_2);
     return NULL;
+}
+void d2common_unload_act(D2Version gameVersion, Act* pAct) {
+    if (gameVersion == VersionPathOfDiablo) return D2COMMON_Pod_UnloadAct(pAct);
+    if (gameVersion == VersionProjectDiablo2) return D2COMMON_Pd2_UnloadAct(pAct);
+    if (gameVersion == VersionDiablo2) return D2COMMON_Pd2_UnloadAct(pAct);
+}
 
+Act* d2common_load_act(D2Version gameVersion, int actId, int seed, int difficulty) {
+    /** Cache hit */
+    if (act_seeds[actId] == seed && act_diff[actId] == difficulty) return acts[actId];
+    /** Seed or difficulty has changed unload old act */
+    if (acts[actId] != NULL) d2common_unload_act(gameVersion, acts[actId]);
+
+    Act* pAct = d2common_load_act_run(gameVersion, actId, seed, difficulty);
+    act_seeds[actId] = seed;
+    act_diff[actId] = difficulty;
+    acts[actId] = pAct;
+
+    return pAct;
 }
 
 LevelTxt* d2common_get_level_text(D2Version gameVersion, int levelCode) {
