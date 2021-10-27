f98b56e260ab8639df15923250faf1f6086872a4
Blayne Chard <blayne@chard.com>
2021-10-27 23:17:09 +1300

    feat(state): basic event structure to change state

--- packages/state/src/state.ts
+++ packages/state/src/state.ts
@@ -1,9 +1,17 @@
 import { Diablo2GameStateJson } from './json.js';
+import { Emitter } from '@servie/events';
 
-export class Diablo2GameState {
+export interface StateEvents {
+  'state:changed': [Diablo2GameState];
+  'state:replace': [Diablo2GameStateJson];
+  'player:move': [{ x: number; y: number }];
+}
+
+export class Diablo2GameState extends Emitter<StateEvents> {
   state: Diablo2GameStateJson;
 
   constructor(id: string) {
+    super();
     this.state = {
       id,
       createdAt: Date.now(),
@@ -24,5 +32,22 @@ export class Diablo2GameState {
       objects: [],
       kills: [],
     };
+
+    this.on('player:move', this.playerMove);
+    this.on('state:replace', this.stateReplace);
   }
+
+  playerMove = (pt: { x: number; y: number }): void => {
+    if (pt.x === this.state.player.x && pt.y === this.state.player.y) return;
+    this.state.player.x = pt.x;
+    this.state.player.y = pt.y;
+
+    this.emit('state:changed', this);
+  };
+
+  stateReplace = (state: Diablo2GameStateJson): void => {
+    // TODO detect if state actually changed
+    this.state = state;
+    this.emit('state:changed', this);
+  };
 }
