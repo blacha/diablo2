84a2415f2c285db8ec33866e22d956c581ef0633
Blayne Chard <blayne@chard.com>
2022-05-11 17:35:56 +1200

    feat(memory): read map seed

--- packages/memory/README.md
+++ packages/memory/README.md
@@ -7,7 +7,8 @@ Memory reader for diablo2
 const player = await d2.scanForPlayer(this.playerName);
 
 const act = await player.act;
-act.mapSeed; // MapSeed
+const actMisc = await act.pActMisc.fetch(d2);
+actMisc.mapSeed; // MapSeed
 
 const path = await player.path;
 // Player X/Y
--- packages/memory/src/struts/d2r.act.ts
+++ packages/memory/src/struts/d2r.act.ts
@@ -6,6 +6,7 @@ const { at, lu32, u8, lu16 } = bp;
 export const D2rActMiscStrut = bp.object('D2rActMisc', {
   tombLevel: at(0x120, lu32),
   difficulty: at(0x830, lu16),
+  mapSeed: at(0x840, lu32),
   pAct: at(0x858, new Pointer(u8)),
   pLevelFirst: at(0x868, new Pointer(u8)),
 });
@@ -16,7 +17,7 @@ export const D2rActStrut = bp.object('Act', {
   // mapSeed: at(0x1c, lu32), // This has been moved
   // unk1: at(0x08, new Pointer(bp.u8)),
   actId: at(0x28, lu32),
-  pActMisc: at(0x78, new Pointer(D2rActMiscStrut)), // this is wrong
+  pActMisc: at(0x78, new Pointer(D2rActMiscStrut)),
 });
 D2rActStrut.setSize(0x80);
 export type ActS = StrutInfer<typeof D2rActStrut>;
